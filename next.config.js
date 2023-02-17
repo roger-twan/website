97100baf76da331ccce8a1b696eb02734069edc2
Roger Twan <roger.twan@gmail.com>
2023-02-17 18:42:33 +0800

    Modify configuration to adapt Github Page
    fix ESlint
    
    Modify configuration to adapt Github Page
    
    Modify configuration to adapt Github Page
    
    Modify configuration to adapt Github Page
    
    Modify configuration to adapt Github Page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    publicAssetsPrefix: getPublicAssetsPrefix()
+  }
+}
+
+function getPublicAssetsPrefix() {
+  const isGithubActions = process.env.GITHUB_ACTIONS || false
+  let prefix = ''
+
+  if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+    prefix = `/${repo}`
+  }
+
+  return prefix
 }
 
 module.exports = nextConfig
