89b79a0cc2021011299fc4f24c38b2a48b8523a4
Roger Twan <roger.twan@gmail.com>
2023-02-24 03:50:29 +0800

    Fix: fix icon import build error

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,8 +1,7 @@
 import Link from 'next/link';
 import { PropsWithChildren } from 'react'
 import style from './layout.module.scss'
-import HomeIcon from '@mui/icons-material/home';
-
+import { Home as HomeIcon } from '@mui/icons-material';
 
 export default function Layout( { children }: PropsWithChildren<{}> ) {
   return (
