b69b8c9fccf173e3e0a8c41c9ba855cff5150851
Roger Twan <roger.twan@gmail.com>
2023-09-13 20:36:10 +0800

    :sparkles: App Portfolio Page

--- pages/portfolio/index.page.tsx
+++ pages/portfolio/index.page.tsx
@@ -1,20 +1,72 @@
 import CommonHeader from '@/components/commonHeader'
 import Layout from '@/components/layout'
 import { ReactElement } from 'react'
+import Image from 'next/image'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Chip,
+  Container,
+  Typography,
+} from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+import projectList, { ProjectInfo } from './projectList.data'
 
-const PageAi = () => {
+const PagePortfolio = () => {
   return (
     <>
       <CommonHeader title="Portfolio" />
-      <div style={{ margin: '200px auto', textAlign: 'center' }}>
-        <h2>In progress</h2>
-      </div>
+      <Container>
+        <Grid container rowSpacing={5} style={{ padding: '5vh 0' }}>
+          {projectList.map((item: ProjectInfo) => (
+            <Grid xs={4} key={item.title}>
+              <Card sx={{ maxWidth: 345 }}>
+                <Image
+                  src={item.img}
+                  style={{ width: '100%', height: 220 }}
+                  alt={item.title}
+                  placeholder="blur"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {item.title}
+                  </Typography>
+                  {item.platforms.map((platform: string) => (
+                    <Chip
+                      key={platform}
+                      size="small"
+                      label={platform}
+                      style={{ marginRight: '4px' }}
+                    />
+                  ))}
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    height={40}
+                  >
+                    {item.techStacks.map((tech: string, index: number) =>
+                      index === item.techStacks.length - 1 ? tech : `${tech} · `
+                    )}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" target="blank" href={item.url}>
+                    View Code
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </>
   )
 }
 
-PageAi.getLayout = (page: ReactElement) => {
+PagePortfolio.getLayout = (page: ReactElement) => {
   return <Layout>{page}</Layout>
 }
 
-export default PageAi
+export default PagePortfolio
