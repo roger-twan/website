89ddad5f1462b0654471065246f88be989bfec3e
Roger Twan <roger.twan@gmail.com>
2024-07-06 16:23:59 -0400

    fix(3d): fix file path error

--- pages/portfolios/3d/house/building/floor1/wall/wall-back.ts
+++ pages/portfolios/3d/house/building/floor1/wall/wall-back.ts
@@ -1,6 +1,6 @@
 import { Mesh, MeshPhongMaterial } from 'three'
 import CONFIG from '../../../_config'
-import generateCubeShape from '@/pages/portfolio/3d/generate-cube-shape'
+import generateCubeShape from '@/pages/portfolios/3d/generate-cube-shape'
 
 const cube = generateCubeShape(
   CONFIG.house.size.width,
