da6f819db39dd03cb357562bbddee38429ea3be3
Roger <roger.twan@gmail.com>
2025-08-05 15:13:31 -0400

    Update Vercel Speed Insights

--- global.d.ts
+++ global.d.ts
@@ -2,4 +2,3 @@ declare module 'markdown-yaml-metadata-parser';
 declare module '@akebifiky/remark-simple-plantuml';
 declare module 'masonry-layout';
 declare module 'imagesloaded';
-declare module '@vercel/speed-insights/next';
